be3ce6f046466d0570ccc981b3a461ff6aba691c
Sebi Kovacs <sebi.kovacs@gmail.com>
2015-10-30 14:36:44 +0200

    Updated event page

--- public/js/event-page.js
+++ public/js/event-page.js
@@ -9,6 +9,26 @@ $(document).ready(function () {
     $form.addClass('event-form--show-fields')
   }
 
+  function makeReservation (e) {
+    var $this = $(this)
+    var $form = $this.parents('.event-form')
+
+    $form.addClass('event-form--loading')
+
+    setTimeout(function() {
+      $form.removeClass('event-form--loading')
+      $form.addClass('event-form--success')
+
+      setTimeout(function() {
+        $form.removeClass('event-form--show-fields')  
+        $form.removeClass('event-form--success')
+      }, 10000);
+    }, 1000);
+
+
+  }
+
 
   $('body').on('click','.btn-toggle-fields', toggleFormFields)
+  $('body').on('click','.btn-reserv', makeReservation)
 })
